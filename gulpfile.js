01c7d90ab776b099ff06e564912ee96180db99db
AK36 <kkya2014@163.com>
2016-04-19 12:48:00 +0800

    no message

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp'),
+	spawn = require('child_process').spawn,
+	exec = require('child_process').exec,
+	connect = require('gulp-connect'),
+	open = require('gulp-open'),
+	paths = {
+		site: {
+			root: '_site/',
+			index: 'index.html'
+		}
+	};
+gulp.task('jekyll', function(cb) {
+	// 编译 Jekyll
+	// exec('jekyll.bat server --watch', function(err) {
+	// 	if (err) return cb(err); // 返回 error
+	// 	cb(); // 完成 task
+	// });
+	spawn('jekyll.bat', ['build', '--watch'], {
+		stdio: 'inherit'
+	});
+	cb();
+});
+
+
+gulp.task('connect', function(cb) {
+	connect.server({
+		root: [paths.site.root],
+		port: '8080'
+	});
+	cb();
+});
+
+gulp.task('open', function(cb) {
+	gulp.src(paths.site.root + paths.site.index).pipe(open('', {
+		url: 'http://localhost:8080/' + paths.site.index
+	}));
+	cb();
+});
+
+gulp.task('default', gulp.series('jekyll', 'connect', 'open'));
